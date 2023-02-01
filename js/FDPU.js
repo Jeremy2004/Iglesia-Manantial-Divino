2cceceabe8351e8b605e1fb2531de725707bcdb5
AFeliz <AlbertFeliz@larasofts.com>
2023-02-01 01:10:38 -0400

    Boton donaciones y secciones ver mas

--- js/FDPU.js
+++ js/FDPU.js
@@ -16,3 +16,9 @@
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems);
   });
+
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
