19f459fb065b9a2d80de25d0c61404fbaeafb37b
AFeliz <software.felizgaro@hotmail.com>
2023-05-01 14:55:32 -0400

    Adaptacion index Mobil
    el logo hay que arreglar el responsive

--- js/FDPU.js
+++ js/FDPU.js
@@ -22,3 +22,11 @@
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
   });
+
+  options = {
+    edge: 'right'
+  }
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
